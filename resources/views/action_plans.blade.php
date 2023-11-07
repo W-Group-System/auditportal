bf6f30bd53b451f55e4a363978b410f9be6ccc1a
renzcabato1 <cabato.renz.renz@gmail.com>
2023-11-07 18:06:09 +0800

    Done

--- resources/views/action_plans.blade.php
+++ resources/views/action_plans.blade.php
@@ -48,7 +48,9 @@
                                                     <li><a title='Update' href="#view{{$action_plan->id}}" data-toggle="modal" >Upload</a></li>
                                                     <li><a title='Change Target Date' href="#change{{$action_plan->id}}" data-toggle="modal" >Change Target Date</a></li>
                                                     <li><a title='Return Action Plan' href="#return{{$action_plan->id}}" data-toggle="modal" >Return Action Plan</a></li>
+                                                    @if($action_plan->attachment != null)
                                                     <li><a title='Closed Action Plan' href="#closed{{$action_plan->id}}" data-toggle="modal" >Close Action Plan</a></li>
+                                                    @endif
                                                     <li><a title='View History' href="#view_history{{$action_plan->id}}" data-toggle="modal" >View History</a></li>
                                                 @endif
                                             </ul>
