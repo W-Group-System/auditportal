9d179c6a802e72ad90c12d970b3352dafda7808d
Mark Ian <bautistamarkian083@gmail.com>
2025-04-25 17:53:51 +0800

    done

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -196,7 +196,7 @@
             </div>
             <div class="footer">
                 <div class='text-right'>
-                    WGROUP DEVELOPER &copy; {{date('Y')}}
+                    WGROUP DEVELOPERsssssssss &copy; {{date('Y')}}
                 </div>
             </div>
         </div>
