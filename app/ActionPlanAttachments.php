73e97171cfecffa20f86bc59f417b8c97c5bd5e1
Mark Ian <bautistamarkian083@gmail.com>
2025-11-20 07:20:03 +0800

    add email notif for auditee

--- app/ActionPlanAttachments.php
+++ app/ActionPlanAttachments.php
@@ -8,4 +8,10 @@ use OwenIt\Auditing\Contracts\Auditable;
 class ActionPlanAttachments extends Model implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
+
+    protected $table = 'action_plan_attachments';
+    protected $fillable = [
+        'action_plan_id',
+        'attachment',
+    ];
 }
